776650f2bc080e7c613629213912e75965e28ff0
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2020-01-23 16:11:39 -0600

    poquita documentción

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
+/**
+ * Este file configura algunos aspectos del entorno
+ * los cuales son:
+ *     -la conexion a la base de datos
+ *     -poner en acción a Express js y el puerto
+ *     -declarar los controllers y sus rutas
+ *     
+ * 
+ */
+
 require('dotenv').config()
 
-const express=require('express')
-const app=express()
-const mongoose= require('mongoose')
+const express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    port = 6454
 
 
 /*CONECCION A MONGO*/
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
-const db= mongoose.connection
-db.on( 'error',(error)=> console.error(error))
-db.once('open',()=>console.log('conectado a MongoDb'))
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('conectado a MongoDb'))
 /*FIN CONECCION A MONGO*/
 
 
-/*USE JSON */
+/*USA JSON */
 app.use(express.json())
 
 
@@ -22,10 +35,12 @@ app.use(express.json())
 
 
 /*RUTEO API*/
-const panRuta=require('./routes/pan') 
-app.use('/pan',panRuta)
+const panRuta = require('./routes/pan')
+
+app.use('/pan', panRuta)
+
 /*FIN RUTEO API*/
 
-const port=6454
 
-app.listen(port,()=> console.log('corre en puerto '+port))
+
+app.listen(port, () => console.log('ejecución en el puerto: ' + port))
