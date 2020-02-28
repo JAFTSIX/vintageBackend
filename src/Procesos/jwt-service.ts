b9a6602248f96219462fa99a823e2bb7e902e8fd
Fabian3830 <49417624+Fabian3830@users.noreply.github.com>
2020-02-27 21:39:40 -0600

    Mejora
    Dure un vergazo para semejante mierda porque se me despicho el proyecto

--- src/Procesos/jwt-service.ts
+++ src/Procesos/jwt-service.ts
@@ -1,4 +1,4 @@
-import {UserProfile} from '@loopback/security';
+import {UserProfile, securityId} from '@loopback/security';
 import {promisify} from 'util';
 import {HttpErrors} from '@loopback/rest';
 import {inject} from '@loopback/core';
@@ -6,12 +6,40 @@ import {TokenServiceBindings} from '../keys';
 import {TokenService} from '@loopback/authentication';
 const jwt = require('jsonwebtoken');
 const signAsync = promisify(jwt.sign);
+const verifyAsync = promisify(jwt.verify);
 
-export class JwtService {
+export class JwtService implements TokenService {
   @inject(TokenServiceBindings.TOKEN_SECRET)
   public readonly jwtSecret: string;
   @inject(TokenServiceBindings.TOKEN_EXPIRES_IN)
   public readonly jwtExpiresIn: string;
+
+  async verifyToken(token: string): Promise<UserProfile> {
+    if (!token) {
+      throw new HttpErrors.Unauthorized(
+        `Error verifying token: 'token' is null`,
+      );
+    }
+
+    let userProfile: UserProfile;
+
+    try {
+      // decode user profile from token
+      const decryptedToken = await verifyAsync(token, this.jwtSecret);
+      // don't copy over  token field 'iat' and 'exp', nor 'email' to user profile
+      userProfile = Object.assign(
+        {[securityId]: '', name: ''},
+        {id: decryptedToken.id, name: decryptedToken.name},
+      );
+    } catch (error) {
+      throw new HttpErrors.Unauthorized(
+        `Error verifying token: ${error.message}`,
+      );
+    }
+
+    return userProfile;
+  }
+
   async generateToken(userProfile: UserProfile): Promise<string> {
     if (!userProfile) {
       throw new HttpErrors.Unauthorized(
